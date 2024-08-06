f842449b9079fe183e2884fb7c08dfaf6bbb9bb5
Jason Walsh <j@wal.sh>
2024-08-06 18:32:58 -0400

    Synchronizing local and codespace

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="boston_python_llm_tokenizer",
-    version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-)
